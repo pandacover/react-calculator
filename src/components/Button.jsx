4ee7480af6c54d2861073fada871960e241735cb
luvmakin01 <57311008+luvmakin01@users.noreply.github.com>
2021-05-19 16:34:37 +0530

    finally got doen some stuff!

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.css';
+
+const isOperator = val => {
+    return !(isNaN(val)) || val === '=' || val === '.'
+}
+
+export const Button = props => <div className={`button-wrapper ${
+    isOperator(props.children)?null : 'operator'}`}
+onClick={() => props.handleClick(props.children)}
+>{props.children}</div>
+
